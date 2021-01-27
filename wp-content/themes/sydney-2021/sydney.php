c47c07c5c35c4ebe6d1e769cc133c2e7a447e9e5
miwanoff <marina.iva.noffa@gmail.com>
2021-01-27 23:36:16 +0200

    change sydney.php

--- wp-content/themes/sydney-2021/sydney.php
+++ wp-content/themes/sydney-2021/sydney.php
@@ -3,8 +3,49 @@
 
 get_header();
 
-while (have_posts()): the_post();
-    the_content();
+// while (have_posts()): the_post();
+//     the_content();
+// endwhile;
+?>
+
+<main id="primary" class="site-main">
+    <section class="articleSpaceBlock container">
+        <h4 class="tittleBlock padleft"><?php bloginfo('name');?></h4>
+        <?php
+if (have_posts()):
+
+    if (is_home() && !is_front_page()):
+    ?>
+        <header>
+            <h1 class="page-title screen-reader-text"><?php single_post_title();?></h1>
+        </header>
+        <?php
+endif;
+
+/* Start the Loop */
+while (have_posts()):
+    the_post();
+
+    /*
+     * Include the Post-Type-specific template for the content.
+     * If you want to override this in a child theme, then include a file
+     * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+     */
+    get_template_part('template-parts/content', get_post_type());
+
 endwhile;
 
+the_posts_navigation();
+
+else:
+
+    get_template_part('template-parts/content', 'none');
+
+endif;
+?>
+    </section>
+
+</main><!-- #main -->
+
+<?php
 get_footer();
