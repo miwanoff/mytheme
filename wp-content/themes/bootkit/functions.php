4e7e5d25e48309a9c7db5202e5d41d987697cdea
miwanoff <marina.iva.noffa@gmail.com>
2021-03-18 02:14:40 +0200

    add Recipe post and recipe_data

--- wp-content/themes/bootkit/functions.php
+++ wp-content/themes/bootkit/functions.php
@@ -103,3 +103,76 @@ function bootkit_ajax()
 //     return $template;
 
 // }
+/* Виджет Bootkit Widget */
+class bootkit_widget extends WP_Widget
+{
+
+    public function __construct()
+    {
+        parent::__construct(
+            // widget ID
+            'bootkit_widget',
+            // widget name
+            __('Simple widget', ' bootkit_widget_domain'),
+            // widget description
+            array('description' => __('Simple widget bootkit', 'bootkit_widget_domain'))
+        );
+    }
+
+    // public function widget($args, $instance)
+    // {
+    //     $title = apply_filters('widget_title', $instance['title']);
+    //     echo $args['before_widget'];
+    //     //if title is present
+    //     if (!empty($title)) {
+    //         echo $args['before_title'] . $title . $args['after_title'];
+    //     }
+
+    //     //output
+    //     echo __('Hello, bootkit', 'bootkit_widget_domain');
+
+    // }
+
+    public function widget($args, $instance)
+    {
+        $title = apply_filters('widget_title', $instance['title']);
+        $blog_title = get_bloginfo('name');
+        $tagline = get_bloginfo('description');
+
+        echo $args['before_widget'] . $args['before_title'] . $title . $args['after_title'];?>
+<p><strong>Site Name:</strong> <?php echo $blog_title ?></p>
+<p><strong>Tagline:</strong> <?php echo $tagline ?></p>
+<?php echo $args['after_widget'];
+    }
+
+    public function form($instance)
+    {
+        if (isset($instance['title'])) {
+            $title = $instance['title'];
+        } else {
+            $title = __('Default header', 'bootkit_widget_domain');
+        }
+
+        ?>
+<p>
+    <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:');?></label>
+    <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>"
+        name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" />
+</p>
+<?php
+}
+
+    public function update($new_instance, $old_instance)
+    {
+        $instance = array();
+        $instance['title'] = (!empty($new_instance['title'])) ? strip_tags($new_instance['title']) : '';
+        return $instance;
+    }
+
+}
+
+function bootkit_register_widget()
+{
+    register_widget('bootkit_widget');
+}
+add_action('widgets_init', 'bootkit_register_widget');
