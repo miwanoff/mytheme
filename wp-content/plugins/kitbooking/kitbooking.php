df28790938f0cb2c655a285abbb77c2b2b1d29c5
miwanoff <marina.iva.noffa@gmail.com>
2021-03-25 12:26:24 +0200

    add recipe widget

---
+++ wp-content/plugins/kitbooking/kitbooking.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Kit Booking
+Plugin URI: https://github.com/miwanoff/kitbook
+Description: Booking plugin.
+Version: 1.0
+Author: MAI
+Author URI: https://github.com/miwanoff/
+License: GPLv2 or later
+Text Domain: kitbooking
+ */
