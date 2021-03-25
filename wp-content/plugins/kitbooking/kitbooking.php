91f9504e54758736b6e703a06783d580f1832901
miwanoff <marina.iva.noffa@gmail.com>
2021-03-25 15:22:49 +0200

    add begining kit-booking plugin

--- wp-content/plugins/kitbooking/kitbooking.php
+++ wp-content/plugins/kitbooking/kitbooking.php
@@ -9,3 +9,23 @@ Author URI: https://github.com/miwanoff/
 License: GPLv2 or later
 Text Domain: kitbooking
  */
+if (!function_exists('add_action')) {
+    die;
+}
+function kitbooking_say_hello()
+{
+    echo "Kitbooking say Hello!";
+}
+add_action('admin_init', 'kitbooking_say_hello');
+
+// class KitBooking
+// {
+//     public function __construct($string)
+//     {
+//         echo $string;
+//     }
+// }
+
+// if (class_exists('KitBooking')) {
+//     new KitBooking("Hello!");
+// }
